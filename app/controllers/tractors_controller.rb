829c798a9e35ddc6eb2624b44a9083d2e0373cf1
Clement Lucien <clement.lucien1@gmail.com>
2020-03-02 16:51:52 +0100

    Creation tractor controller + methode index show new create and tractor params

---
+++ app/controllers/tractors_controller.rb
@@ -0,0 +1,30 @@
+class TractorsController < ApplicationController
+
+  def index
+    @tractors = Tractor.all
+  end
+
+  def show
+    @tractor = Tractor.find(params[:id])
+  end
+
+  def new
+    @tractor = Tractor.new
+  end
+
+  def create
+    @tractor = Tractor.new(tractor_params)
+    if @restaurant.save
+      redirect_to tractor_path(@tractor)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:tractor).permit(:photo, :title, :descritpion, :price_per_day)
+  end
+
+end
