513a8d0cb752907040455f31367f6da214082374
kevin-calligaro <calligaro.kevin@gmail.com>
2020-03-02 18:02:04 +0100

    accepted and decline method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'owners/index'
+  get 'owners/show'
+  get 'owners/add'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
