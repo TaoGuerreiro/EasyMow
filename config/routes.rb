7da637cbc41ed74e7bb085bb012770ffee163ab9
kevin-calligaro <calligaro.kevin@gmail.com>
2020-03-02 16:44:25 +0100

    add routes to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#tractors'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  namespace :owners do
+    resources :tractors, only: [:index]
+    resources :bookings, only: [:index] do
+      member do
+        patch 'accept'
+        patch 'refuse'
+      end
+    end
+  end
+
+  resources :tractors, only: [:index, :show, :new, :create]
+  resources :bookings, only: [:index, :new, :create]
 end
