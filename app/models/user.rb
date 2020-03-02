611d242f3bc9f0f23ab032f50f9105919b5e91ef
kevin-calligaro <calligaro.kevin@gmail.com>
2020-03-02 17:00:39 +0100

    fix master user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-rails g devise:viewsclass User < ApplicationRecord
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
