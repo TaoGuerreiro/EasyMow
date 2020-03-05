80cf6a07aabaf67306464e08a6f57b8b48e784fe
Florent Guilbaud <florent.guilbaud@gmail.com>
2020-03-05 07:49:02 +0100

    fix deploy

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,11 +1,20 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
+  const textNavbar = document.querySelectorAll('.target');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= 380) {
         navbar.classList.add('bg-light');
+        textNavbar.forEach((line) => {
+          line.classList.remove('text-light');
+          line.classList.add('text-success');
+        });
       } else {
         navbar.classList.remove('bg-light');
+        textNavbar.forEach((line) => {
+          line.classList.add('text-light');
+          line.classList.remove('text-success');
+        });
       }
     });
   }
